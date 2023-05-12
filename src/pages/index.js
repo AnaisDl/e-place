890c220d76078c8a699b1c6cf7ffcbc8e8f233e7
Anais Druelle <anais.druelle@epita.fr>
2023-05-12 18:48:52 +0200

    Step1: get config AND canvas

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { calculateLayout } from "./utils";
 
 import * as streams from "../utils/streams";
 import { connectStream } from "../utils/socket-communication";
-import { getConfig } from "../utils/api-communication";
+import { getCanvas, getConfig } from "../utils/api-communication";
 
 // Initialize the layout
 calculateLayout();
@@ -16,5 +16,9 @@ const socket = streams.initSocket();
 await connectStream(socket);
 
 // Get configuration
-const config = await getConfig();
-console.log(config);
+const config = (await getConfig())["data"];
+console.log("Config: ", config);
+
+// Get canvas
+const canvas = (await getCanvas())["data"]["pixels"];
+console.log("Canvas: ", canvas);
--- src/utils/api-communication.js
+++ src/utils/api-communication.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export async function getConfig() {
     return await axios.get(`${import.meta.env.VITE_URL}/api/rooms/epi-place/config`);
 }
+
+export async function getCanvas() {
+    return await axios.get(`${import.meta.env.VITE_URL}/api/rooms/epi-place/canvas`);
+}
